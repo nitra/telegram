d48951bcdc621ea5c9f9789a4ce69eeab88bd60a
Pavlo <paveldurachevskyi1987@gmail.com>
2024-10-04 13:49:19 +0300

    TELEGRAM_ROUND_CLOCK

--- npm/src/index.js
+++ npm/src/index.js
@@ -8,15 +8,17 @@ export const MAX_TELEGRAM_MSG_LENGTH = 4096
 
 export const sendMessage = async (text, params) => {
   const currentHour = new Date().getHours()
-  if (currentHour >= 8 && currentHour <= 18) {
+  if (process.env.TELEGRAM_ROUND_CLOCK || (currentHour >= 8 && currentHour <= 18)) {
     // Max length of a Telegram message is 4096 characters
     if (text >= MAX_TELEGRAM_MSG_LENGTH) {
       text = text.slice(0, MAX_TELEGRAM_MSG_LENGTH)
     }
 
-    let url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${env.TELEGRAM_CHAT_ID}&text=${text}`
+    let url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${
+      env.TELEGRAM_CHAT_ID
+    }&text=${encodeURIComponent(text)}`
 
-    if (params.parse_mode.toLowerCase() === 'html') {
+    if (params?.parse_mode?.toLowerCase() === 'html') {
       url += '&parse_mode=HTML'
     }
     let res
