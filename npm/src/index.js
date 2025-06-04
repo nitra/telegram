cf1804861c127b1789b8c955ab4eae9cc5529a13
vitaliytv <v@nitra.ai>
2025-06-04 16:45:07 +0300

    . Внесено зміни в логіку відправки повідомлень у Telegram, зокрема, видалено обмеження на години роботи для відправки повідомлень.

--- npm/src/index.js
+++ npm/src/index.js
@@ -8,31 +8,35 @@ export const MAX_TELEGRAM_MSG_LENGTH = 4096
 
 export const sendMessage = async (text, params) => {
   const currentHour = new Date().getHours()
-  if (process.env.TELEGRAM_ROUND_CLOCK || (currentHour >= 8 && currentHour <= 18)) {
-    // Max length of a Telegram message is 4096 characters
-    if (text >= MAX_TELEGRAM_MSG_LENGTH) {
-      text = text.slice(0, MAX_TELEGRAM_MSG_LENGTH)
-    }
-
-    let url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${
-      env.TELEGRAM_CHAT_ID
-    }&text=${encodeURIComponent(text)}`
-
-    if (params?.parse_mode?.toLowerCase() === 'html') {
-      url += '&parse_mode=HTML'
-    }
-    let res
-    try {
-      res = await fetch(url)
-    } catch (error) {
-      log.error(error)
-      return false
-    }
-    if (res.status >= 400) {
-      log.error('Telegram message skipped, not sent ', text)
-      return false
-    }
-  } else {
-    log.info('Telegram message skipped, not in working hours: ', text)
+  // if (process.env.TELEGRAM_ROUND_CLOCK || (currentHour >= 8 && currentHour <= 18)) {
+  // Max length of a Telegram message is 4096 characters
+  if (text >= MAX_TELEGRAM_MSG_LENGTH) {
+    text = text.slice(0, MAX_TELEGRAM_MSG_LENGTH)
   }
+
+  let url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${
+    env.TELEGRAM_CHAT_ID
+  }&text=${encodeURIComponent(text)}`
+
+  if (params?.parse_mode?.toLowerCase() === 'html') {
+    url += '&parse_mode=HTML'
+  }
+
+  if (!(currentHour >= 8 && currentHour <= 18)) {
+    url += '&disable_notification=true'
+  }
+  let res
+  try {
+    res = await fetch(url)
+  } catch (error) {
+    log.error(error)
+    return false
+  }
+  if (res.status >= 400) {
+    log.error('Telegram message skipped, not sent ', text)
+    return false
+  }
+  // } else {
+  //   log.info('Telegram message skipped, not in working hours: ', text)
+  // }
 }
